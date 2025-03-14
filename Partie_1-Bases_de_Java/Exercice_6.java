9b069d144da77db22f01f0b6dc59362d80f28e5a
samy-belazzoug <samy.belazzoug@laplateforme.io>
2025-03-14 11:31:23 +0100

    Exercice 6 Done.

---
+++ Partie_1-Bases_de_Java/Exercice_6.java
@@ -0,0 +1,12 @@
+//6. Vérifier si un nombre est pair ou impair.
+ class Exercice_6 {
+    public static void main(String[] args) {
+        int nombre = 12;
+        if (nombre % 2 == 0) {
+            System.out.println("Pair");
+        }
+        else {
+            System.out.println("Impair");
+        }
+    }
+}
