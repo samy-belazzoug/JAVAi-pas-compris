06643cd9987a16c0f81b8c65b644dcfbe177d6d0
samy-belazzoug <samy.belazzoug@laplateforme.io>
2025-03-14 12:04:58 +0100

    Exercice 10 Done.

---
+++ Partie_1-Bases_de_Java/Exercice_10.java
@@ -0,0 +1,16 @@
+//10. Vérifier si un nombre est positif, négatif ou nul.
+public class Exercice_10 {
+    public static void main(String[] args) {
+        int x = 0;
+        if (x > 0) {
+            System.out.println("Positif");
+        }
+        else if (x == 0) {
+            System.out.println("Nul");
+        }
+        else {
+            System.out.println("Négatif");
+        }
+
+    }
+}
