fbb6a0ddd68becf18382fbe6062f1765621ea706
samy-belazzoug <samy.belazzoug@laplateforme.io>
2025-03-14 10:54:03 +0100

    Exercice 4 Done.

---
+++ Partie_1-Bases_de_Java/Exercice_4.java
@@ -0,0 +1,13 @@
+//4. Effectuer des opérations arithmétiques de base (+, -, \*, /, %).
+public class Exercice_4 {
+    public static void main(String[] args) {
+    int add = 5+2;
+    int sous = 7-5;
+    int mult = 2*7;
+    float div = 14/9;
+    System.out.println("5 + 2 = " + add);    
+    System.out.println("7 - 5 = " + sous);  
+    System.out.println("2 x 7 = " + mult);  
+    System.out.println("14 / 9 = " + div);
+    }
+}
